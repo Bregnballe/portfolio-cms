ec506e689d56706c60d5f8633287b0679147a457
Bregnballe <bregnballe@gmail.com>
2024-03-04 12:24:21 +0100

    renamed name to be more descriptive

--- src/blocks/GalleryImage.ts
+++ src/blocks/GalleryImage.ts
@@ -5,19 +5,59 @@ export const GalleryImage: Block = {
 	interfaceName: "GalleryImage", // optional
 	fields: [
 		{
-			name: "PrimaryImage",
+			name: "primaryImagePosition",
+			label: "Primary Image Position",
+			type: "select",
+			defaultValue: "top",
+			options: [
+				{
+					label: "Top",
+					value: "top",
+				},
+				{
+					label: "Right",
+					value: "right",
+				},
+				{
+					label: "Bottom",
+					value: "bottom",
+				},
+				{
+					label: "Left",
+					value: "left",
+				},
+			],
+		},
+		{
+			name: "aspectRatio",
+			label: "Aspect Ratio",
+			type: "select",
+			defaultValue: "1/1",
+			options: [
+				{
+					label: "1/1",
+					value: "1/1",
+				},
+				{
+					label: "3/4",
+					value: "3/4",
+				},
+			],
+		},
+		{
+			name: "primaryImage",
 			label: "Primary Image",
 			type: "upload",
 			relationTo: "media",
 		},
 		{
-			name: "SecondaryImage",
+			name: "secondaryImage",
 			label: "Secondary Image",
 			type: "upload",
 			relationTo: "media",
 		},
 		{
-			name: "TertiaryImage",
+			name: "tertiaryImage",
 			label: "Tertiary Image",
 			type: "upload",
 			relationTo: "media",
